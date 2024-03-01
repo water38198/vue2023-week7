0a5557c2c9c24f353c52405cac3205a9909f05cc
PayRoom <water38198@gmail.com>
2024-03-02 01:50:13 +0800

    修正VITE問題

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 /* global process */
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +19,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:process.env.NODE_ENV === 'production' ? '/vue2023-week7/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/vue2023-week7/' : '/',
+    // 錯誤修正 參考:https://israynotarray.com/other/20220928/1377668819/
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  },
 })
